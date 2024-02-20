27d4e8b14933af07f062f76eba7c6b5e53f029d4
isaiabarzoi <isaiabarzoi@gmail.com>
2024-02-20 12:10:32 +0100

    working with question component logic

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
 import QUESTION from "../questions";
 import TimerQuestion from "./TimerQuestion";
 
 export default function Question({ index, onHandleAnswer }) {
+  const [answerState, setAnswerState] = useState({
+    answerSelected: "",
+    isCorrect: null,
+  });
+
+  const handleSelectAnswer = (answer) => {
+    setAnswerState({
+      answerSelected: answer,
+      isCorrect: null,
+    });
+
+    setTimeout(() => {
+      setAnswerState({
+        answerSelected: answer,
+        isCorrect: QUESTION[index].answers[0] === answer,
+      });
+      setTimeout(() => {
+        onHandleAnswer(answer);
+      }, 2000);
+    }, 1000);
+  };
+
+  let answerCorrect = "";
+
+  if (answerState.answerSelected && answerState.isCorrect !== null) {
+    answerCorrect = answerState.isCorrect ? "correct" : "wrong";
+  } else if (answerState.answerSelected) {
+    answerCorrect = "selected";
+  }
+
   return (
     <div id="question">
       <TimerQuestion
@@ -12,9 +43,27 @@ export default function Question({ index, onHandleAnswer }) {
       <h2>{QUESTION[index].text}</h2>
       <ul id="answers">
         {QUESTION[index].answers.map((answer) => {
+          const isSelected = answerState.answerSelected === answer;
+          let cssClass = "";
+
+          if (answerCorrect === "selected" && isSelected) {
+            cssClass = answerCorrect;
+          }
+
+          if (answerCorrect === "correct" && isSelected) {
+            cssClass = answerCorrect;
+          } else if (answerCorrect === "wrong" && isSelected) {
+            cssClass = answerCorrect;
+          }
+
           return (
             <li key={answer} className="answer">
-              <button onClick={() => onHandleAnswer(answer)}>{answer}</button>
+              <button
+                onClick={() => handleSelectAnswer(answer)}
+                className={cssClass}
+              >
+                {answer}
+              </button>
             </li>
           );
         })}
